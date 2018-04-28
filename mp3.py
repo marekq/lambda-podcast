642b6265757f2d0a56e2cc332374fab9c02147d0
marekq <marek.kuczynski@gmail.com>
2018-04-28 21:12:28 +0200

    v1.0 - simplified the code dramatically, removed third party libraries to speed up code, removed the dynamodb dependancy

--- mp3.py
+++ mp3.py
@@ -1,213 +1,62 @@
-import urllib2, boto3, time, eyed3, os
-from lxml import etree
+#!/usr/bin/python
+# marek kuczynski
+# @marekq
+# www.marek.rocks
+# coding: utf-8
 
-###########
-
-# create dictionaries 
-fdict			= dict() 	# store file size of s3 keys
-mdict			= dict()	# store modified date of s3 keys
-hdict 			= dict() 	# store hashes of s3 keys
-
-
-# iterate over all files in S3 that are mp3, get filesize and modified date
-def get_all_files():
-	# create session to s3 music bucket
-	s 			= boto3.session.Session()
-	c			= s.client('s3', region_name = os.environ['s3_region_name']) 
-	a			= c.list_objects(Bucket = os.environ['s3_musicbucket'])
-
-	# create session to s3 webbucket
-	e 			= s3_session(os.environ['s3_webbucket'])
-
-	# create session to dynamodb	
-	d			= boto3.resource('dynamodb', region_name = os.environ['s3_region_name']).Table(os.environ['dynamo_table'])
-
-	# touch an empty file for S3 file redirection - perhaps there is an easier way? 
-	f 	= open('/tmp/s3.txt', 'w')
-	f.write('')
-	f.close()
-
-	for x in a['Contents']:
-		if 'mp3' in x['Key']:
-			
-			# write the size and modified date of the MP3 to dictionaries
-			fdict[x['Key']]	= x['Size']
-			mdict[x['Key']] = x['LastModified']
-			hdict[x['Key']] = x['ETag'].strip('"')
-
-			get_file(x['Key'], c, d, e)
-
-
-# check dynamo if a particular ETag was analyzed already
-def check_dynamo(mp3hash, fkey, d):
-	t 			= d.get_item(TableName = os.environ['dynamo_table'], Key = {'mp3hash': mp3hash})
-
-	if t.has_key('Item'):
-		print 'HIT!', t['Item']['artist'], t['Item']['title']
-		return t['Item']['artist'], t['Item']['title']
-  
-	else:
-		a, t 	= get_id3(fkey, d)
-		print 'MISS!', a, t
-		return a, t
-
-
-# write the ID3 attributes to DynamoDB so it doesnt need to be recalculated every lambda run 
-def write_dynamo(mp3hash, title, artist, fkey, d):
-	#b 			= boto3.resource('dynamodb', region_name = os.environ['s3_region_name'])
-
-	d.put_item(TableName = os.environ['dynamo_table'], Item = {
-		'mp3hash' : mp3hash,
-		'title' : artist,
-		'artist' : title,
-		's3-loc' : fkey
-	})
-
-
-# read ID3 tags of the file by downloading the first 1kb from S3
-def get_id3(fkey, d):
-	eyed3.log.setLevel("ERROR")
-
-	s 			= boto3.session.Session()
-	c			= s.client('s3', region_name = os.environ['s3_region_name']) 
-	r 			= c.get_object(Bucket = os.environ['s3_musicbucket'], Key = fkey, Range = 'bytes=0-1024')	
-	fn 			= fkey.split('/')[-1]
-	mp3hash 	= hdict[fkey]
-
-	print 'added '+fkey+' to dynamodb'
-
-	# write the first kb of the mp3 file to disk
-	f			= open('/tmp/'+fn, 'w')
-	f.write(r['Body'].read())
-	f.close()
-
-	# check the MP3 file for ID3 tags (artist, title)
-	a			= eyed3.load('/tmp/'+fn)
-	
-	try:
-		art 	= a.tag.artist
-		track 	= a.tag.title
-	
-	except:
-		art, track 	= fkey.split('/')
-
-	write_dynamo(mp3hash, art, track, fkey, d)
-	return art, track
-
-
-# convert datetime input to proper formatted timestamp
-def return_times(x):
-	return x.strftime("%a, %d %b %Y %H:%M:%S %Z")
-
-
-# generate a presigned URL and create XML entry for file
-def get_file(fkey, c, d, e):
-
-	# get the ETag for the file
-	mp3hash		= hdict[fkey]
-
-	# create presigned URL for the MP3
-	z 			= c.generate_presigned_url('get_object', Params = {'Bucket': os.environ['s3_musicbucket'], 'Key': fkey}, ExpiresIn = os.environ['link_expiry'])
-	
-	# create a website redirect object in your public S3 bucket to shorten the presigned URL character length and prettify the MP3 URL
-	bkey 		= os.environ['podcast_folder']+'/'+mp3hash[:10]+'.mp3'
-	put_s3(e, os.environ['s3_webbucket'], '/tmp/s3.txt', bkey, z, 'audio/mpeg')
-	
-
-	# option 1 - use s3 path and trackname for podcast track properties instead of checking id3 tag in dynamo - do not store the results in dynamo
-	if os.environ['enable_id3'] == 'False' or os.environ['enable_id3'] == 'false':
-		art, track 	= fkey.split('/')
-		print 's3-filename', fkey, art, track
-
-	# option 2 - check dynamo if the mp3 hash was analyzed already for id3 tags - if not, analyse now and write results to dynamo
-	else:	
-		art, track 	= check_dynamo(mp3hash, fkey, d)
-		print 'dynamo-cache', fkey, art, track
-
-
-	item		= etree.SubElement(channel, 'item')
-	desc 		= etree.SubElement(item, 'description')
-	artist		= etree.SubElement(item, 'artist')
-	title		= etree.SubElement(item, 'title')
-	pubd		= etree.SubElement(item, 'pubDate')
-	size 		= etree.SubElement(item, 'size')
-	enc			= etree.SubElement(item, 'enclosure', url = os.environ['podcast_url']+'/'+bkey, length = str(fdict[fkey]), type = 'audio/mpeg')
-
-	desc.text	= str(art)+' - '+str(track)
-	artist.text	= art
-	title.text	= track
-	pubd.text	= return_times(mdict[fkey])
-	size.text	= str(fdict[fkey])
-
+from xml.etree.ElementTree import Element, SubElement, Comment, tostring 
+import urllib2, boto3, time, os
 
 # create the XML document root
 def make_root():
-	global rss, channel 
-	nam			= {'atom' : 'http://www.w3.org/2005/Atom', 'itunes' : 'http://www.itunes.com/dtds/podcast-1.0.dtd'}
-	rss 		= etree.Element('rss', nsmap = nam, version = '2.0')
+	rss 		= Element('rss', version = '2.0')
+	rss.set('atom', 'http://www.w3.org/2005/Atom')
+	rss.set('itunes', 'http://www.itunes.com/dtds/podcast-1.0.dtd')
 
-	channel		= etree.SubElement(rss, 'channel')
-	title 		= etree.SubElement(channel, 'title')
-	author		= etree.SubElement(channel, 'author')
-	desc 		= etree.SubElement(channel, 'description')
-	link		= etree.SubElement(channel, 'link')
-	lang		= etree.SubElement(channel, 'language')
-	build		= etree.SubElement(channel, 'lastBuildDate')
-	pubd 		= etree.SubElement(channel, 'pubDate')
+	channel 	= SubElement(rss, 'channel')
+	SubElement(channel, 'title').text	= os.environ['podcast_name']
+	SubElement(channel, 'author').text = os.environ['podcast_author']
+	SubElement(channel, 'description').text = os.environ['podcast_desc']
+	SubElement(channel, 'link').text = os.environ['podcast_url']
+	SubElement(channel, 'language').text = 'en-us'
+	SubElement(channel, 'lastBuildDate').text = time.strftime("%a, %d %b %Y %H:%M:%S %Z")
+	SubElement(channel, 'pubDate').text = time.strftime("%a, %d %b %Y %H:%M:%S %Z")
 
-	title.text	= os.environ['podcast_name']
-	author.text	= os.environ['podcast_author']
-	desc.text	= os.environ['podcast_desc']
-	link.text 	= os.environ['podcast_url']
-	lang.text	= 'en-us'
+	image		= SubElement(channel, 'image')
+	SubElement(image, 'url').text = os.environ['podcast_img']
+	SubElement(image, 'title').text = os.environ['podcast_desc']
+	SubElement(image, 'link').text = os.environ['podcast_url']
 
-	pubd.text 	= return_times(time)
-	build.text	= return_times(time)
+	# create session to s3 music bucket
+	s 	= boto3.session.Session()
+	c	= s.client('s3', region_name = os.environ['s3_region_name']) 
+	a	= c.list_objects(Bucket = os.environ['s3_musicbucket'])
 
-	image		= etree.SubElement(channel, 'image')
-	imgurl		= etree.SubElement(image, 'url')
-	imgtit		= etree.SubElement(image, 'title')
-	imglin		= etree.SubElement(image, 'link')
+	# check all mp3 files in the bucket
+	for x in a['Contents']:
+		if 'mp3' in x['Key']:
+		
+			# create presigned URL for the MP3
+			z 			= c.generate_presigned_url('get_object', Params = {'Bucket': os.environ['s3_musicbucket'], 'Key': x['Key']}, ExpiresIn = os.environ['link_expiry'])
+			art, track 	= x['Key'].split('/')
+			
+			item		=SubElement(channel, 'item')
+			SubElement(item, 'description').text = str(art)+' - '+str(track)
+			SubElement(item, 'artist').text = art
+			SubElement(item, 'title').text = track
+			SubElement(item, 'pubDate').text = x['LastModified'].strftime("%a, %d %b %Y %H:%M:%S %Z")
+			SubElement(item, 'size').text = str(x['Size'])
+			SubElement(item, 'enclosure', url = z, length = str(x['Size']), type = 'audio/mpeg')
 
-	imgurl.text	= os.environ['podcast_img']
-	imgtit.text = os.environ['podcast_desc']
-	imglin.text	= os.environ['podcast_url']
-
-
-def s3_session(bucketn):
-	s 			= boto3.resource('s3')
-	return s.Bucket(os.environ['s3_webbucket'])
-
-
-# store a file on a S3 bucket
-def put_s3(session, bucketn, fbody, fkey, redir, contentt):
-	if len(redir) != 0:
-		session.put_object(Body = open(fbody), Key = fkey, ContentType = contentt, WebsiteRedirectLocation = redir)
-	else:
-		session.put_object(Body = open(fbody), Key = fkey, ContentType = contentt)
-
-	
-# prettify the xml and write to disk
-def prettify_xml():
-	x 			= etree.tostring(rss, xml_declaration = True, encoding = 'utf-8', pretty_print = True)
-	f 			= open('/tmp/rss.xml', 'w')
-	f.write(x)
+	# write the output rss.xml file to /tmp
+	f 	= open('/tmp/rss.xml', 'wb')
+	f.write(tostring(rss))
 	f.close()
 
+	# upload the rss.xml file to 3
+	s = boto3.resource('s3').Bucket(os.environ['s3_webbucket'])
+	s.put_object(Body = open('/tmp/rss.xml'), Key = 'podcast.xml', ContentType = 'application/xml', ACL = 'public-read')
 
 # lambda handler
 def handler(event, context):
-	# create the root XML structure
 	make_root()
-	
-	# iterate all files in the music S3 bucket, add URL's and ID3 metadata to the XML
-	get_all_files()
-	
-	# prettify the XML so its easier to read for humans
-	prettify_xml()
-	
-	# get an s3 session
-	s 			= s3_session(os.environ['s3_musicbucket'])
-
-	# write the XML file to the public S3 bucket
-	put_s3(s, os.environ['s3_webbucket'], '/tmp/rss.xml', os.environ['s3_res_file'], '', 'application/xml')
--- readme.md
+++ readme.md
@@ -10,7 +10,7 @@ Technical Description
 ---------------------
 
 
-The Lambda function can run periodically to add/remove podcast files from the S3 bucket containing your music. The XML file read by your podcast client can optionally be written to a different S3 bucket with webhosting enabled. Metadata about analyzed MP3's (i.e. filesize, ID3 tags) are cached in a DynamoDB table to speed up podcast generation. 
+The Lambda function can run periodically to add/remove podcast files from the S3 bucket containing your music. The XML file read by your podcast client can optionally be written to a different S3 bucket with webhosting enabled. 
 
 
 The diagram below shows the technical components of lambda-podcast;
@@ -22,7 +22,7 @@ The diagram below shows the technical components of lambda-podcast;
 Besides generating public podcasts, the tool can be used to synchronize music onto an iPhone/iPad using a podcast app such as DownCast instead of needing iTunes. I personally use it as such to have all my music and mixtapes available for streaming and/or downloading across all my mobile devices and laptops. By eliminating the need for a webserver to host the podcast contents, you can run the podcast service for very low cost and with no periodic maintenance requirements (i.e. patching, running services, setting cronjobs etc.). 
 
 
-A 128 MB Lambda function with a 60 second timeout should be sufficient up to a 1000 MP3's if you rely on the folder structure of your S3 bucket for naming. If you want to use ID3 tags from the MP3 files, the Lambda function will run significantly longer as it does need to download and analyze the first kilobyte of every MP3 file first, but I'll work on a better solution for this. 
+A 128 MB Lambda function with a 10 second timeout should be sufficient up to a 1000 MP3's if you rely on the folder structure of your S3 bucket for naming.
 
 
 
@@ -37,13 +37,11 @@ Installing lambda-podcast on your AWS account can be done as follows;
 
 2. Create an S3 bucket which will host the podcast XML file - you can also use the same music bucket from step 1 to do this. This bucket should be publically accessible whereas the music bucket does not need to be exposed as it uses S3 signed URL's. 
 
-3. Create a DynamoDB table with a read/write capacity of one to store the MP3 metadata. This way the Lambda function doesnt need to analyze already known MP3's more, leading to a much quicker podcast generation. 
+3. Configure the parameters in serverless.yml with details about your S3 buckets and details about the podcast. Your Lambda role must be able to read from the music bucket and write to the bucket where the XML will be posted. It will also need read/write access to DynamoDB. 
 
-4. Configure the parameters in serverless.yml with details about your S3 buckets and details about the podcast. Your Lambda role must be able to read from the music bucket and write to the bucket where the XML will be posted. It will also need read/write access to DynamoDB. 
+4. Use 'serverless deploy' to push the Python code to a Lambda function. Test the Lambda function manually to ensure the S3 buckets can be reached and that a valid XML is created in the destination bucket. Use CloudWatch logs of the Lambda function to debug any issues with permissions or wrong environment variables. 
 
-5. Use 'serverless deploy' to push the Python code to a Lambda function. Test the Lambda function manually to ensure the S3 buckets can be reached and that a valid XML is created in the destination bucket. Use CloudWatch logs of the Lambda function to debug any issues with permissions or wrong environment variables. 
-
-6. Add a trigger to run the Lambda function whenever a file is uploaded/deleted in the music bucket. This means files will be  available in the podcast whenever a file is added a few seconds later. As an alternative, you can also auto regenerate the podcast every 24 hours or so. 
+5. Add a trigger to run the Lambda function whenever a file is uploaded/deleted in the music bucket. This means files will be  available in the podcast whenever a file is added a few seconds later. As an alternative, you can also auto regenerate the podcast every 24 hours or so to keep the file fresh. 
 
 
 You can now add the XML's URL to your favourite podcast reader and stream music from your S3 bucket!
@@ -56,8 +54,6 @@ Contents of repository
 
 - The Lambda function code is in mp3.py, this file also contains the handler which is used to trigger the function whenever a change happens in the S3 music bucket. Set your Lambda handler to "mp3.handler" to trigger the function. 
 
-- The LXML and eyeD3 folders contain Python libraries for generation of XML and identification of ID3 tags. These libraries were built on the Amazon Linux AMI and are precompiled for you to use in the Lambda function directly - simply include them in the ZIP uploaded to the Lambda function. 
-
 - The serverless.yml file contains a template for deployment using the Serverless toolkit - this needs to be further expanded so that all components can be deployed using this template automatically (DynamoDB, create S3 buckets, etc.) as it only takes care of the Lambda function now. 
 
 
@@ -67,8 +63,6 @@ Pending feature requests
 
 
 - Implement inclusion of podcast links not hosted on personal S3 buckets (i.e. SoundCloud, third party podcasts). This would make the lambda-podcast a onestop shop for podcasts/mixtapes with the flexibility to stream or download music as you wish from one app. 
-
-- Explore if native Python libraries available in Lambda can be used instead of bundling them with the code. This would eliminate any future compatability issues (i.e. switching to Python 3) and reduce the size of the codebase. 
 
 - Make the podcast feed iTunes compatible so that it could be used for official iTunes podcasts - it is currently unknown if there are hard roadblocks to do so. Some requirements include specific iTunes XML tags and ensuring the SSL certificate is recognized by iTunes. 
 
@@ -85,6 +79,8 @@ Completed feature requests
 - Create a CloudFormation template and ZIP which would preconfigure all components for you, eliminating the need for a manual setup. To do so, Lambda environment variables need to be included as parameters for the podcast instead of having to modify variables by hand in the Python source code.
 
 - Cache the results from ID3 analysis into DynamoDB so files dont need to be analyzed every time the podcast is regenerated. This will drive down cost and makes the tool a lot more scalable.
+
+- Explore if native Python libraries available in Lambda can be used instead of bundling them with the code. This would eliminate any future compatability issues (i.e. switching to Python 3) and reduce the size of the codebase. 
 
 
 
