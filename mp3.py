15bb99c0f8c94fcfa954ce2d9eb54b027362fa1e
marekq <marek.kuczynski@gmail.com>
2018-04-29 20:53:08 +0200

    added the project to AWS serverless repository!
    - added SAM template
    - removed old serverless template
    - renamed and removed variables
    - added license file
    
    todo; fix IAM permissions in SAM template

--- mp3.py
+++ mp3.py
@@ -1,61 +1,69 @@
 #!/usr/bin/python
-# marek kuczynski
-# @marekq
+# marek kuczynski, @marekq
 # www.marek.rocks
-# coding: utf-8
+# source; https://github.com/marekq/lambda-podcast
 
 from xml.etree.ElementTree import Element, SubElement, Comment, tostring 
-import urllib2, boto3, time, os
+import boto3, os, time
+
+# the following environment variables are set - I'll put these as SAM variables in the future
+podcast_name	= 'My serverless podcast'		
+podcast_author	= 'AWS lambda'				
+podcast_desc	= 'My collection of mp3 files'	
+podcast_url		= '.'
+podcast_img 	= '.'
+link_expiry 	= '86400'					
+s3_bucket		= os.environ['s3_bucket']
 
 # create the XML document root
 def make_root():
+	# add XML headers 
 	rss 		= Element('rss', version = '2.0')
 	rss.set('atom', 'http://www.w3.org/2005/Atom')
 	rss.set('itunes', 'http://www.itunes.com/dtds/podcast-1.0.dtd')
 
+	# add a channel containing the details of the podcast
 	channel 	= SubElement(rss, 'channel')
-	SubElement(channel, 'title').text	= os.environ['podcast_name']
-	SubElement(channel, 'author').text = os.environ['podcast_author']
-	SubElement(channel, 'description').text = os.environ['podcast_desc']
-	SubElement(channel, 'link').text = os.environ['podcast_url']
+	SubElement(channel, 'title').text = podcast_name
+	SubElement(channel, 'author').text = podcast_author
+	SubElement(channel, 'description').text = podcast_desc
+	SubElement(channel, 'link').text = podcast_url
 	SubElement(channel, 'language').text = 'en-us'
 	SubElement(channel, 'lastBuildDate').text = time.strftime("%a, %d %b %Y %H:%M:%S %Z")
 	SubElement(channel, 'pubDate').text = time.strftime("%a, %d %b %Y %H:%M:%S %Z")
 
+	# add a podcast image which the podcast client will display
 	image		= SubElement(channel, 'image')
-	SubElement(image, 'url').text = os.environ['podcast_img']
-	SubElement(image, 'title').text = os.environ['podcast_desc']
-	SubElement(image, 'link').text = os.environ['podcast_url']
+	SubElement(image, 'url').text = podcast_img
+	SubElement(image, 'title').text = podcast_desc
+	SubElement(image, 'link').text = podcast_url
 
 	# create session to s3 music bucket
-	s 	= boto3.session.Session()
-	c	= s.client('s3', region_name = os.environ['s3_region_name']) 
-	a	= c.list_objects(Bucket = os.environ['s3_musicbucket'])
+	s	= boto3.client('s3')
+	a	= s.list_objects_v2(Bucket = s3_bucket)
 
 	# check all mp3 files in the bucket
 	for x in a['Contents']:
-		if 'mp3' in x['Key']:
-		
+		if 'mp3' in x['Key'] and  '/' in x['Key']:
+
 			# create presigned URL for the MP3
-			z 			= c.generate_presigned_url('get_object', Params = {'Bucket': os.environ['s3_musicbucket'], 'Key': x['Key']}, ExpiresIn = os.environ['link_expiry'])
+			z 			= s.generate_presigned_url('get_object', Params = {'Bucket': s3_bucket, 'Key': x['Key']}, ExpiresIn = link_expiry)
+			
+			# use the folder name as the artist name and the filename as the track name
 			art, track 	= x['Key'].split('/')
 			
-			item		=SubElement(channel, 'item')
+			item		= SubElement(channel, 'item')
 			SubElement(item, 'description').text = str(art)+' - '+str(track)
 			SubElement(item, 'artist').text = art
-			SubElement(item, 'title').text = track
+			SubElement(item, 'title').text = track.split('.')[0]
 			SubElement(item, 'pubDate').text = x['LastModified'].strftime("%a, %d %b %Y %H:%M:%S %Z")
 			SubElement(item, 'size').text = str(x['Size'])
 			SubElement(item, 'enclosure', url = z, length = str(x['Size']), type = 'audio/mpeg')
+			SubElement(item, 'guid').text = x['Key'].strip()
 
-	# write the output rss.xml file to /tmp
-	f 	= open('/tmp/rss.xml', 'wb')
-	f.write(tostring(rss))
-	f.close()
-
-	# upload the rss.xml file to 3
-	s = boto3.resource('s3').Bucket(os.environ['s3_webbucket'])
-	s.put_object(Body = open('/tmp/rss.xml'), Key = 'podcast.xml', ContentType = 'application/xml', ACL = 'public-read')
+	# upload the rss.xml file to S3 - in the future, include "ACL = 'public-read'" as a flag to publish the XML file
+	s.put_object(Bucket = s3_bucket, Body = tostring(rss), Key = 'podcast.xml', ContentType = 'application/xml')
+	print('uploaded XML document of '+str(len(tostring(rss)))+' bytes to https://'+s3_bucket+'.s3.amazonaws.com/podcast.xml')
 
 # lambda handler
 def handler(event, context):
